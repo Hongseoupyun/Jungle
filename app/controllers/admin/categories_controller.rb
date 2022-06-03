3765e59983c59f66e2bd4bb5b80517e442c5228e
Hongseoup Yun <simonhongseoupyun02156@gmail.com>
2022-06-03 15:48:34 -0400

    Add initial file for routes, views and controllers for admin/categories

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,15 @@
+class Admin::CategoriesController < ApplicationController
+  http_basic_authenticate_with name: ENV['ADMIN_USER'], password: ENV['ADMIN_PASSWORD']
+
+  def index
+    
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+
+end
